f4f78dd0fde1345e6c8d885b60d581c2fe215bee
DejonGill <dejonjgill@gmail.com>
2016-05-10 16:57:26 -0700

    Changed port for production

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var path = require('path');
 var httpProxy = require('http-proxy');
 var publicPath = path.resolve(__dirname, 'public');
-
-var port = 3000;
+var isProduction = process.env.NODE_ENV === 'production';
+var port = isProduction ? process.env.PORT : 3000;
 
 // We need to add a configuration to our proxy server,
 // as we are now proxying outside localhost
@@ -19,21 +19,41 @@ app.use(express.static(publicPath));
 //server/compiler.js runs webpack-dev-server which creates the bundle.js which index.html serves
 //the compiler adds some console logs for some extra sugar
 //notice that you will not see a physical bundle.js because webpack-dev-server runs it from memory
-var bundle = require('./server/compiler.js')
-bundle()
+var bundle = require('./server/compiler.js');
+bundle();
 
 //express now processes all requests to localhost:8080
 //app.all is a special routing method used for loading middleware functions
 app.all('/build/*', function (req, res) {
   proxy.web(req, res, {
-      target: 'http://localhost:8080'
-  })
-})
+    target: 'http://localhost:8080'
+  });
+});
 
 proxy.on('error', function(e) {
-  console.log('Could not connect to proxy, please try again...')
+  console.log('Could not connect to proxy, please try again...', e);
 });
 
 app.listen(port, function () {
-  console.log('Server running on port ' + port)
+  console.log('Server running on port ' + port);
+});
+
+
+  //if we're not in production, this proxies requests to localhost:3000 and sends them to our webpack server at localhost:8080
+if (isProduction) {
+  var bundle = require('./server/compiler.js');
+  bundle();
+  app.all('/build/*', function (req, res) {
+    proxy.web(req, res, {
+      target: 'http://localhost:8080'
+    });
+  });
+}
+
+proxy.on('error', function(e) {
+  console.log('Could not connect to proxy, please try again...', e);
+});
+//catch all other get requests, such as when refreshing
+app.get('*', function(req, res) {
+  res.sendFile(publicPath + '/index.html');
 });
