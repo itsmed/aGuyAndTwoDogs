5daf9ca9dcf750451e418abd059f0844e13980aa
DejonGill <dejonjgill@gmail.com>
2016-05-10 12:56:14 -0700

    Added some content

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -13,8 +13,8 @@ export default class About extends Component {
       </div>
       <div className='about-content-wrapper'>
         <h2 className='about-content'>About Me</h2>
-          <p className='about-content'>I’m a full stack software engineer who works with customers and design teams to build rich user-facing applications. In my spare time I enjoy working on personal as well as open source projects. I like finding new technologies, and am always looking for new tools to increase development efficiency. </p>
-          <p className='about-content'>I am passionate about tech, education and civis. My hobbies include working on projects that blend the three together. I also enjoy spending time with my dogs, hiking, and making pie.</p>
+        <p className='about-content'>I’m a full stack software engineer who works with customers and design teams to build rich user-facing applications. In my spare time I enjoy working on personal as well as open source projects. I like finding new technologies, and am always looking for new tools to increase development efficiency. </p>
+        <p className='about-content'>I am passionate about tech, education and civis. My hobbies include working on projects that blend the three together. I also enjoy spending time with my dogs, hiking, and making pie.</p>
       </div>
     </div>;
   }
