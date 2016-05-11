662957d173a61d6ab63029256f36f8a11a5ba065
DejonGill <dejonjgill@gmail.com>
2016-05-11 13:33:29 -0700

    Added contact field.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -12,13 +12,13 @@ export default class Header extends Component {
     return <div>
       <div className="header-background"></div>
       <div className='header'>
-        <div className='logo'></div>
         <h1>A Guy And Two Dogs</h1>
       </div>
       <ul> 
-      <li className='social-links'><a href="https://www.linkedin.com/in/dejongill"><i className="fa fa-linkedin-square fa-camera-retro fa-3x link-thumb"></i></a></li>
-      <li className='social-links'><a href="https://github.com/itsmed" target="_blank"><i className="fa fa-github-square fa-camera-retro fa-3x link-thumb"></i></a></li>
-      <li className='social-links'> <a href="https://twitter.com/dejongill" target="_blank"><i className="fa fa-twitter-square fa-3x link-thumb"></i></a></li>
+      <li className='social-links'><a href='https://www.linkedin.com/in/dejongill'><i className='fa fa-linkedin-square fa-camera-retro fa-3x social-links'></i></a></li>
+      <li className='social-links'><a href='https://github.com/itsmed' target='_blank'><i className='fa fa-github-square fa-camera-retro fa-3x social-links'></i></a></li>
+      <li className='social-links'> <a href='https://twitter.com/dejongill' target='_blank'><i className='fa fa-twitter-square fa-3x social-links'></i></a></li>
+      <li className='social-links'><a href='mailto:dejonjgill@gmail.com' className='social-links'>Contact</a></li>
           </ul>
     </div>;
   }
